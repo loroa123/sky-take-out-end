5b403640dc79d0bf40484ac44280c3d3f4fb7196
loroa123 <llorry.theone@gmail.com>
2026-01-13 23:50:39 +0800

    springtask，订单状态定时处理

--- sky-server/src/main/java/com/sky/task/OrderTask.java
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -21,11 +21,13 @@ public class OrderTask {
 
     /**
      * 处理超时订单的方法
+     * 每分钟0秒的时候出发，*表示每
      */
     @Scheduled(cron = "0 * * * * ? ") //每分钟触发一次
     public void processTimeoutOrder(){
         log.info("定时处理超时订单：{}", LocalDateTime.now());
 
+        // 计算出time=当前时间减去15min
         LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
 
         // select * from orders where status = ? and order_time < (当前时间 - 15分钟)
@@ -43,11 +45,13 @@ public class OrderTask {
 
     /**
      * 处理一直处于派送中状态的订单
+     * 其实可以优化一下店铺打样的时候进行处理
      */
     @Scheduled(cron = "0 0 1 * * ?") //每天凌晨1点触发一次
     public void processDeliveryOrder(){
         log.info("定时处理处于派送中的订单：{}",LocalDateTime.now());
 
+        //找凌晨十二点前且处于派送中的订单，也就是减去60min
         LocalDateTime time = LocalDateTime.now().plusMinutes(-60);
 
         List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
